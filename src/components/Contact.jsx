fefec7b8d5987178d729e32fd89017e438c850af
daincoding <official.toyou@gmail.com>
2025-01-29 20:46:04 +0100

    Contact Component adjusted

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,6 @@ const Contact = () => {
         <h2 className='my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-4xl text-transparent'>Get In Touch</h2>
         <div className='text-center tracking-tighter'>
             <p className='my-4'>Location: {CONTACT.location}</p>
-            <p className='my-4'>Phone: {CONTACT.phoneNo}</p>
             <a type='mail' href='mailto:alexander.panske@yahoo.com' className='my-10 border-b'>{CONTACT.email}</a>
         </div>
     </motion.div>
